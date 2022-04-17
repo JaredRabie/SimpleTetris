73c112614f60c285457047081ff2343280d473e8
Jared Rabie <jared.rabie@gmail.com>
2022-04-17 21:28:04 +1200

    Added new tetronimos; tetronimos show after hitting bottom; creation of placed_tetronimo_grid class

--- main.py
+++ main.py
@@ -3,9 +3,29 @@ import os
 from variables import *
 
 #!SEE variables.py FOR GLOBAL VARIABLE DEFINITIONS
-pygame.display.set_caption("Tetris")
 
-class tetronimo_stack:
+class placed_tetronimo_grid:
+    def __init__(self):
+        self.__grid = grid
+
+    def place(self, current_tetronimo):
+        for brick in current_tetronimo:
+            brick_col = (brick.x//brick_width) - 1 
+            brick_row = 19 - ((brick.y//brick_width) - 1)
+            self.__grid[brick_row][brick_col] = current_tetronimo.get_colour()        
+    
+    def display(self):
+        for row in range(0, 19):
+            for col in range(0, 10):
+                if self.__grid[row][col] != None:
+                    x_coord = (col + 1)*brick_width
+                    y_coord = (20 - row)*brick_width
+                    brick = pygame.Rect(x_coord, y_coord, brick_width, brick_width)
+                    colour = self.__grid[row][col]
+                    pygame.draw.rect(window, colour, brick)
+                
+
+class tetronimo_stack_class:
     def __init__(self):
         self.__items = []
     
@@ -21,7 +41,6 @@ class tetronimo_stack:
     def remove_top(self):
         top_item = self.__items[0]
         self.__items = self.__items[1:]
-        top_item -= game_border.width
         return top_item
     def remove_bottom(self):
         bottom_item = self.__items[-1]
@@ -38,19 +57,28 @@ class tetronimo_stack:
     def size(self):
         return len(self.__items)
     
-    def populate(self, size, hitbox_list):
-        hitbox_list = hitbox_list*(size//len(hitbox_list) + 1) #avoids IndexError by lengthening hitbox_list
+    def populate(self, size, hitbox_generators):
         while size > 0:
-            self.add_bottom(tetronimo(hitbox_list.pop(0)))
+            colour = hitbox_generators[size % len(hitbox_generators)][-1] #taking colour from final element
+            lst = hitbox_generators[size % len(hitbox_generators)][:len(hitbox_generators)+1] #taking all tuples (everything but colour)
+            hitbox = []
+            for item in lst:
+                print(item)
+                hitbox.insert(0, pygame.Rect(tetronimo_bench_x + item[0]*brick_width, tetronimo_bench_y + item[1]*brick_width, brick_width, brick_width))
+            hitbox.append(colour)
+            self.add_bottom(tetronimo_class(hitbox))
             size -= 1
 
-class tetronimo():
+class tetronimo_class():
     def __init__(self, hitbox):
-        self.__bricks = hitbox
+        self.__bricks = hitbox[:4]
+        self.__colour = hitbox[4]
         self.__location = 0
 
         self.x = min([brick.x for brick in self.__bricks])
+        self.y = min([brick.y for brick in self.__bricks])
         self.width = max([brick.x for brick in self.__bricks]) - min([brick.x for brick in self.__bricks]) + brick_width
+        self.height = max([brick.y for brick in self.__bricks]) - min([brick.y for brick in self.__bricks]) + brick_width
     
     #Overriding the "for __ in __:" iterator to move individual bricks
     def __iter__(self):
@@ -74,40 +102,65 @@ class tetronimo():
         self.x = min([brick.x for brick in self.__bricks])
         return self
     
-    def display(self):
+    def move_down(self):
         for brick in self.__bricks:
-            pygame.draw.rect(window, (0, 255, 0), brick)
+            brick.y += brick_width
+            self.y = min([brick.y for brick in self.__bricks])
 
-def movement_handler(keys_pressed, current_tetronimo, movement_timer):
-    if movement_timer > movement_cooldown:
-        if keys_pressed[pygame.K_LEFT] or keys_pressed[pygame.K_RIGHT]: #Check if we need to reset the movement_timer (movement_timer resets if the object moves to limit the speed of each object)
+    def get_colour(self):
+        if self.__colour == "Green":
+            colour = green
+        if self.__colour == "Yellow":
+            colour = yellow
+        if self.__colour == "Turquoise":
+            colour = turquoise
+        return colour
+
+    def display(self):
+        colour = self.get_colour()
+        for brick in self.__bricks:
+            pygame.draw.rect(window, colour, brick)
+
+def movement_handler(keys_pressed, current_tetronimo, lateral_timer, vertical_timer):
+    if lateral_timer > movement_cooldown:
+        if keys_pressed[pygame.K_LEFT] or keys_pressed[pygame.K_RIGHT]: #Check if we need to reset the lateral_timer (lateral_timer resets if the object moves to limit the speed of each object)
             if keys_pressed[pygame.K_LEFT] and current_tetronimo.x > game_border.x:
                 current_tetronimo -= brick_width
-                movement_timer = 0
+                lateral_timer = 0
 
             if keys_pressed[pygame.K_RIGHT] and current_tetronimo.x + current_tetronimo.width < game_border.x + game_border.width:
                 current_tetronimo += brick_width
-                movement_timer = 0
+                lateral_timer = 0
 
     if keys_pressed[pygame.K_DOWN]:
-        print() #Downward script in development
+        pass
     
-    return movement_timer + 1
+    if vertical_timer > vert_movement_cooldown:
+        current_tetronimo.move_down()
+        vertical_timer = 0
 
-def draw_window(current_tetronimo):
+    return lateral_timer + 1, vertical_timer + 1
+
+def draw_window(current_tetronimo, tetronimos_placed):
     window.fill(white)
     pygame.draw.rect(window, black, game_border)
     current_tetronimo.display()
+    tetronimos_placed.display()
     pygame.display.update()
 
 def main():
-    tetronimos = tetronimo_stack()
-    tetronimos.populate(20, hitbox_list)
+    tetronimo_stack = tetronimo_stack_class()
+    tetronimo_stack.populate(20, hitbox_generators)
+    current_tetronimo = tetronimo_stack.remove_top()
+    current_tetronimo -= game_border.width
 
+    tetronimos_placed = placed_tetronimo_grid()
+
+    pygame.display.set_caption("Tetris")
     clock = pygame.time.Clock()
     run = True
-    movement_timer = 0
-    current_tetronimo = tetronimos.remove_top()
+    lateral_timer = 0
+    vertical_timer = 0
     while run:
         clock.tick(fps)
         for event in pygame.event.get():
@@ -115,9 +168,15 @@ def main():
                 run = False
         
         keys_pressed = pygame.key.get_pressed()
-        movement_timer = movement_handler(keys_pressed, current_tetronimo, movement_timer)
+        lateral_timer, vertical_timer = movement_handler(keys_pressed, current_tetronimo, lateral_timer, vertical_timer)
+        if current_tetronimo.y + current_tetronimo.height > 20*brick_width:
+            tetronimos_placed.place(current_tetronimo)
+            current_tetronimo = tetronimo_stack.remove_top()
+            current_tetronimo -= game_border.width
+
+
         
-        draw_window(current_tetronimo)
+        draw_window(current_tetronimo, tetronimos_placed)
     pygame.quit()
 
 main()
--- variables.py
+++ variables.py
@@ -4,14 +4,40 @@ brick_width = 50
 white = (255,255,255)
 black = (0,0,0)
 fps = 60
+vert_movement_cooldown = 60
 movement_cooldown = 15     #Number of frames before movement is allowed again (Prevents player moving too quickly since keypresses are polled with 60fps)
 
 window = pygame.display.set_mode((1920, 1080))
 game_border = pygame.Rect(brick_width, brick_width, brick_width*10, brick_width*20)
 tetronimo_bench_x, tetronimo_bench_y = game_border.x + game_border.width, game_border.y #coordinates that tetronimos are stored before being put into play
 
-green_hitbox = [pygame.Rect(tetronimo_bench_x, tetronimo_bench_y, brick_width, brick_width),\
-                pygame.Rect(tetronimo_bench_x, tetronimo_bench_y + brick_width, brick_width, brick_width),\
-                pygame.Rect(tetronimo_bench_x + brick_width, tetronimo_bench_y + brick_width, brick_width, brick_width),\
-                pygame.Rect(tetronimo_bench_x + brick_width, tetronimo_bench_y + 2*brick_width, brick_width, brick_width)]
-hitbox_list = [green_hitbox]
+green = (0, 255, 0)
+yellow = (255,255,0)
+turquoise = (64,224,208)
+
+green_hitbox_generator = [(0,0),(0,1),(1,1),(1,2), "Green"]
+yellow_hitbox_generator = [(0,0), (0,1),(1,0),(1,1), "Yellow"]
+turquoise_hitbox_generator = [(0,0),(0,1),(0,2),(0,3),"Turquoise"]
+
+hitbox_generators = [green_hitbox_generator, yellow_hitbox_generator, turquoise_hitbox_generator]
+
+
+grid = [[None, None, None, None, None, None, None, None, None, None],\
+                       [None, None, None, None, None, None, None, None, None, None],\
+                       [None, None, None, None, None, None, None, None, None, None],\
+                       [None, None, None, None, None, None, None, None, None, None],\
+                       [None, None, None, None, None, None, None, None, None, None],\
+                       [None, None, None, None, None, None, None, None, None, None],\
+                       [None, None, None, None, None, None, None, None, None, None],\
+                       [None, None, None, None, None, None, None, None, None, None],\
+                       [None, None, None, None, None, None, None, None, None, None],\
+                       [None, None, None, None, None, None, None, None, None, None],\
+                       [None, None, None, None, None, None, None, None, None, None],\
+                       [None, None, None, None, None, None, None, None, None, None],\
+                       [None, None, None, None, None, None, None, None, None, None],\
+                       [None, None, None, None, None, None, None, None, None, None],\
+                       [None, None, None, None, None, None, None, None, None, None],\
+                       [None, None, None, None, None, None, None, None, None, None],\
+                       [None, None, None, None, None, None, None, None, None, None],\
+                       [None, None, None, None, None, None, None, None, None, None],\
+                       [None, None, None, None, None, None, None, None, None, None]]
